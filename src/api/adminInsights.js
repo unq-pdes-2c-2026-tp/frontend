8c0add347459b1bfcb34c1d2bb8fa95a88580b7d
Pablo Pissi <pablopissi@gmail.com>
2026-09-20 22:42:06 -0300

    Cambio ruta de top spenders

--- src/api/adminInsights.js
+++ src/api/adminInsights.js
@@ -2,6 +2,6 @@ import { apiAuthenticatedClient } from "./ApiRequests";
 
 export const getTopSpenderUsers = () => {
   return apiAuthenticatedClient.get(
-    "/package-purchases/top-spenders/?range=this_month",
+    "/purchases/top-spenders/?range=this_month",
   );
 };
