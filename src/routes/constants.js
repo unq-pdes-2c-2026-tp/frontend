aa869d7d5596c70857ff4b2c1082c443262cd752
Pablo Pissi <pablopissi@gmail.com>
2026-09-20 22:07:39 -0300

    Agrego pagina de home de admins y  card con top usuarios

--- src/routes/constants.js
+++ src/routes/constants.js
@@ -1,6 +1,7 @@
 export const ROUTES = {
   AGENCIES: "/agencies",
   ADMIN_AGENCIES: "/admin-agencies",
+  ADMIN_HOME: "/admin-home",
   LOGIN: "/login",
   PACKAGES: "/packages",
   PROFILE: "/profile",
